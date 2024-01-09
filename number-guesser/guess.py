c2700cc9f46c1bc5edd08e490e21b60c10ec78e6
D-Yakubov <yakubovdastonbek@gmail.com>
2024-01-09 22:20:53 +0500

    play till player wins

--- number-guesser/guess.py
+++ number-guesser/guess.py
@@ -3,7 +3,8 @@ import random
 number = random.randint(0, 10)
 guess = int(input("Guess a number between 0 and 10: "))
 
-if number == guess:
-    print("Topdingiz!")
-else:
-    print("Topa olmadingiz! Yana urinib ko'ring.")
+while True:
+    
+    if number == guess:
+        print("Topdingiz!")
+        break
